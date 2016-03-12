72a54c05f37314dd02f75c79fd6c1f4af1ee7e3e
Shyamala Umamaheswaran <shykart2203@gmail.com>
2016-03-12 10:33:42 +0100

    Adds initial files

---
+++ oauthsample/src/main/java/com/ennovate/OauthsampleApplication.java
@@ -0,0 +1,12 @@
+package com.ennovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthsampleApplication.class, args);
+	}
+}
